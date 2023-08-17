7ec961f56206090e9070ee43f73fa7b7258a7393
Markus Scheck <scheck@esa.tu-darmstadt.de>
2023-08-17 13:11:44 +0200

    fix AMO tests

--- tools/customTests/test_multi_core_amoand/test_multi_core_amoand.c
+++ tools/customTests/test_multi_core_amoand/test_multi_core_amoand.c
@@ -2,21 +2,20 @@
 #include "../print.h"
 #include "../csr.h"
 
-volatile uint_xlen_t mutex = 0;
-uint_xlen_t cnt = 0;
+volatile int cnt = -1;
+volatile int hartid_max = 0;
+volatile int arrived = 0;
 
 volatile int arr[] = {65, 47, 362, 455, 868, 22, 5, 6, 33, 1, 9, 5, 77, 14, 4, 978};
 volatile int amount = 16;
 
 int main() {
-	volatile int cnt = 0;
-	volatile int hartid_max = 0;
-	volatile int arrived = 0;
-	
 	__asm__ volatile ("amomax.w.aq    x0, %0, (%1)"  
 		                          : /* output: register %0 */
 		                          : "r" (csr_read_mhartid())  /* input : register */
 		                          , "r" ( &hartid_max ));
+	
+	for(register int i = 0; i < 100; i++);
 	
 	register int i = 0;
 	while((i*(hartid_max+1)+csr_read_mhartid()) < amount) {
@@ -35,7 +34,7 @@ int main() {
 	while(arrived != hartid_max+1);
 	
 	if(csr_read_mhartid() == 0) {
-		register int control = 0;
+		register int control = -1;
 		for(register int i = 0; i < amount; i++) control &= arr[i];
 		writeToCtrl(RETL, cnt==control);
 		setIntr();
