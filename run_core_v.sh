af93f434c2a7d8b5cc95b2afb7e9078d8dd7882e
Markus Scheck <scheck@esa.tu-darmstadt.de>
2023-10-19 12:41:53 +0200

    always save core-v logs

--- run_core_v.sh
+++ run_core_v.sh
@@ -1,13 +1,27 @@
 cd tools/core-v-verif
 
+. /opt/cad/mentor/2020-21/scripts/QUESTA-CORE-PRIME_2020.4_RHELx86.sh
+export QUESTA_HOME=/opt/cad/mentor/2020-21/RHELx86/QUESTA-CORE-PRIME_2020.4/questasim/
+
+export PROJECT_ROOT=$(pwd)
+export ROOT_PROJECT=$(pwd)
+
+export RISCV=/opt/riscv/
+export RISCV_PREFIX=$RISCV/bin/riscv64-unknown-elf-
+export RISCV_OBJCOPY=$RISCV/bin/riscv64-unknown-elf-objcopy
+export RISCV_GCC=$RISCV/bin/riscv64-unknown-elf-gcc
+export CV_SW_PREFIX=riscv64-unknown-elf-
+
+export DV_TARGET=cv32a6_imac_sv0
+
 test_scripts=(dv-riscv-tests dv-riscv-arch-test dv-riscv-compliance dv-riscv-csr-access-test dv-generated-tests benchmark coremark)
-mkdir -p core_v_logs
+mkdir -p core-v-logs
 
 for script in ${test_scripts[@]}
 do
 	rm -rf scoooter/sim/out* &> /dev/null
 	printf "$script "
-	list_num=99 source ./scoooter/regress/$script.sh 2>&1 | tee >(grep -q "\[FAILED\]") >(grep -q -i "error") > core_v_logs/$script.log
+	list_num=99 source ./scoooter/regress/$script.sh 2>&1 | tee >(grep -q "\[FAILED\]") >(grep -q -i "error") > core-v-logs/$script.log
 	
 	if [ $? -eq 0 ] 
 	then 
